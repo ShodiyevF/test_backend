828ec324fa2b2a27590c2222552d34244d75e314
ShodiyevF <fayzulloh6757@gmail.com>
2023-11-30 11:09:16 +0500

    add cors options

--- server.js
+++ server.js
@@ -40,6 +40,14 @@ const mockCars = [
 
 const app = express()
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/cars', (req, res) => {
     res.json(mockCars)
 })
